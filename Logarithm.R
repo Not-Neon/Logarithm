3b36b3b58fef5a4f006f9b2e5580591e0a9c6434
Neon <82705218+NotNeonMEC@users.noreply.github.com>
2022-04-07 21:32:45 +0530

    Add files via upload

---
+++ Logarithm.R
@@ -0,0 +1,15 @@
+log10 <- function(target_number)
+{
+  log(target_number, base=10)
+}
+
+logE <- function(target_number)
+{
+  E = 2.718281828459045
+  log(target_number, base=E)
+}
+
+logNtoX <- function(base, target_number)
+{
+  log(target_number, base=base)
+}
